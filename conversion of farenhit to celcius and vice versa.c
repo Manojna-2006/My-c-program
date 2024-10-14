6cd89757ebde6580cbad27b2e1b92d4405044def
Manojna-2006 <24b01a05c5@svecw.edu.in>
2024-10-14 20:52:26 +0530

    Add files via upload

---
+++ conversion of farenhit to celcius and vice versa.c
@@ -0,0 +1,12 @@
+#include<stdio.h>//c program to convert farenhit into celcius and vice versa
+int main()
+{ float c,f;
+ printf("enter farenhit value");
+ c=(5*(f-32))/9;
+ printf("%f is the value of celcius", c);
+ f=(c*(9/5))+32;
+ printf("%f is the value of farenhit", f);
+ 
+ return 0;
+ }
+ 
