d5dd5b8939603b467d291fa2e818110c9ebecddd
Manojna-2006 <24b01a05c5@svecw.edu.in>
2024-10-15 09:44:14 +0530

    Add files via upload

---
+++ decimal to binary .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{   int n,binary=0,rem=0,place =1;
+    printf("enter a decimal number");
+    scanf("%d",&n);
+    printf("\nbinaryequivalentof %d is:",n);
+    while(n)
+    {
+        rem=n%2;
+        n=n/2;
+        binary=binary+(rem*place);
+        place=place*10;
+    }
+    printf("%d",binary);
+}
